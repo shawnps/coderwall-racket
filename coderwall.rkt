b3479d9f72de2ca5a04c0d0fb54f362b40f96bb9
Shawn Smith <shawnpsmith@gmail.com>
2012-05-12 23:00:06 -0700

    add team, neighbor, and team-member structs

--- coderwall.rkt
+++ coderwall.rkt
@@ -7,3 +7,7 @@
 
 (define (get-team-json team-id)
   (read-json (get-pure-port (make-team-url team-id))))
+
+(struct team (id score name rank neighbors team-members))
+(struct neighbor (id score name rank))
+(struct team-member (name username badges_count endorsements_count))
